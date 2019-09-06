ef4b65fd8ff23bb5ef768ad3c39416a321b178ff
Krigermus <zyjinxx@hotmail.com>
2019-09-06 11:29:06 +0200

    FetchGetAll

--- src/main/java/rest/MovieResource.java
+++ src/main/java/rest/MovieResource.java
@@ -57,7 +57,7 @@ public class MovieResource {
 
         em.close();
 
-        return "Data added";
+        return "{\"msg\":\"Data added\"}";
     }
 
     @Path("/getAllMovies")
--- src/main/webapp/MovieJS.js
+++ src/main/webapp/MovieJS.js
@@ -1,25 +1,48 @@
-window.onload = function() {
+window.onload = function () {
 
     function getAll() {
-        let url = "https://melif.dk/Movie/api/Movie/getAllMovies";
+        let url = "http://localhost:8080/Movie/api/Movie/getAllMovies";
         startFetch(url, "all");
+    }
+
+    function getData() {
+        let url = "http://localhost:8080/Movie/api/Movie/databaseData";
+        startFetch(url, "data");
     }
 
 
     function startFetch(url, type) {
-        switch (type) {
-            case "all":
-                fetch(url)
-                    .then(res => res.json())
-                    .then(data => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                switch (type) {
+                    case "all":
+                        document.getElementById("data").innerHTML = '';
                         document.getElementById("movie").innerHTML =
-                        "<table><thead><tr><th>Id</th><th>Year</th><th>Name</th></tr><th>Actors</th></thead><tbody>" + 
-                        data.map(x => "<tr><td>" + x.id + "</td><td>" + x.year +"</td><td>" + x.name +"</td><td>" + x.actors.join() +"</td></tr>").join('');
+                            "<table><thead><tr><th>Id</th><th>Year</th><th>Name</th><th>Actors</th></tr></thead><tbody>" +
+                            data.map(x => "<tr><td>" + x.id + "</td><td>" + x.year + "</td><td>" + x.name + "</td><td>" + x.actors.join(", ") + "</td></tr>").join('');
                         + "</tbody></table>";
-                    })
+                        break;
+                    case "single":
+                        document.getElementById("data").innerHTML = '';
+                        document.getElementById("movie").innerHTML =
+                            "<table><thead><tr><th>Id</th><th>Year</th><th>Name</th><th>Actors</th></tr></thead><tbody>" +
+                            data.map(x => "<tr><td>" + x.id + "</td><td>" + x.year + "</td><td>" + x.name + "</td><td>" + x.actors.join(", ") + "</td></tr>").join('');
+                        + "</tbody></table>";
+                        break;
+                    case "data":
+                        document.getElementById("movie").innerHTML = '';
+                        document.getElementById("data").innerHTML =
+                            data.msg;
+                        break;
                 }
-            }
-document.getElementById("getAllMovies").onclick = getAll;
+            })
+    }
+    document.getElementById("getAllMovies").onclick = getAll;
+    document.getElementById("getMovieById").onclick = getAll;
+    document.getElementById("getMovieByName").onclick = getAll;
+    document.getElementById("getCount").onclick = getAll;
+    document.getElementById("createData").onclick = getData;
 
 }
 
